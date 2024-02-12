a6ea7b111fd754057e2797be374dbcd3825b1e05
Valentin Román Aquino <vaquino@frba.utn.edu.ar>
2024-02-12 15:47:07 -0300

    set up

---
+++ src/test/java/db/ContextTest.java
@@ -0,0 +1,13 @@
+package db;
+
+import io.github.flbulgarelli.jpa.extras.test.SimplePersistenceTest;
+import org.junit.jupiter.api.Test;
+
+public class ContextTest implements SimplePersistenceTest {
+
+  @Test
+  void contextUp() {
+  }
+
+
+}
